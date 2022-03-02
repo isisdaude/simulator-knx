8decd920cb4fc423a0f6752f39753de494f38eca
leoalz <71744574+leoalz@users.noreply.github.com>
2022-03-02 19:29:23 +0100

    simple_simulator.py file added

--- device.py
+++ device.py
@@ -4,26 +4,34 @@ Gather the class definitions for the simulated KNX devices
 
 
 class Device:
-    def __init__(self, name, id, location, status):
+    def __init__(self, device_type, name, id, location, status): #The constructor is also a good place for imposing various checks on attribute values
+        self.device_type = device_type  # 'sensor' 'actuator' 'system'
         self.name = name
         self.id = id
         self.location = location
         self.status = status  # status determine if sensor is activated or not, kind of ON/OFF
 
+    def __repr__(self):
+        return f"Device({self.device_type!r}, {self.name!r}, {self.id!r}, {self.location!r}, {self.status!r})"
+
+    def __str__(self):
+        return f"DEvice: {self.device_type} {self.name} {self.id} {self.location} {self.status}"
+
 
 class Sensor(Device):
-    def __init__(self, sensor_type):
-        super().__init__(name, id, location, status)
+    def __init__(self, device_type, name, id, location, status, sensor_type):
+        super().__init__(device_type, name, id, location, status)
         self.sensor_type = sensor_type  # active or passive, just to add a specific argument to class sensor
 
 
 
 class Actuator(Device):
-    def __init__(self, act_state):
-        super().__init__(name, id, location, status)
+    def __init__(self, device_type, name, id, location, status,  actuator_type):
+        super().__init__(device_type, name, id, location, status)
+        self.actuator_type = actuator_type
 
-        
-        
+
+
 class SysDevice(Device):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, device_type, name, id, location, status):
+        super().__init__(device_type, name, id, location, status)
---
+++ simple_simulator.py
@@ -0,0 +1,13 @@
+from device import *
+# device_type, name, id, location, status
+
+sensor1 = Sensor('sensor', 'GePro Brightness Sensor', 'M-0091_A111', '1/1/1', 'ON', 'Brightness sensor')
+
+actuator1 = Actuator('actuator', 'GePro Light', 'M-0091_B222', '1/1/1', 'ON', 'Light')
+
+sysdev1 = SysDevice('system', 'Line Coupler', 'M-0000_C333', '1/1/1', 'ON')
+
+
+print(sensor1)
+print(actuator1)
+print(sysdev1)
