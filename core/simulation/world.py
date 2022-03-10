d54393ba93c1890fac9eb578162c557bea7b6de5
Sapino <isis.daude@epfl.ch>
2022-03-10 19:53:26 +0100

    added scheduler

--- core/simulation/world.py
+++ core/simulation/world.py
@@ -1,9 +1,24 @@
 """
 Some class definitions for the simulation of the physical world
 """
+import schedule, time
 
 class World:
     pass #TODO: should contain an instance of Time for clock ticking
 
 class Time:
-    pass #TODO: implement a clock
+    ''' Clock ticks every X amount of time '''
+    VIRTUAL_INTERVAL = 60
+    ''' Real time that passed between 2 clock ticks '''
+    PHYSICAL_INTERVAL = 3600
+
+    def add_task(task):
+        schedule.every(Time.VIRTUAL_INTERVAL)
+    
+    def remove_task():
+        pass
+
+    def bigbang():
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
