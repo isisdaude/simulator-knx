3124d7400c31c6e251977294e565ed9a8de7e0ba
leoalz <71744574+leoalz@users.noreply.github.com>
2022-06-09 03:17:02 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 """ Program starting hook. """
 
 import sys
-from simulator import launch_simulation 
 sys.path.append("./simulator")
+from simulator import launch_simulation 
 
 if __name__ == "__main__":
     launch_simulation(sys.argv)
