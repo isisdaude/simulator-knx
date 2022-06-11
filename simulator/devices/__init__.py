69f26480df9928fb379a7a16e54a4e9fd8fdac83
leoalz <71744574+leoalz@users.noreply.github.com>
2022-06-11 20:14:31 +0200

    remove dev_tools file, update init package deef

--- simulator/devices/__init__.py
+++ simulator/devices/__init__.py
@@ -1,6 +1,7 @@
-# __init__.py for devices package
+"""
+Package device that gathers all abstract and concrete class definitions for system sevices.
+"""
 from .device_abstractions import *
 from .actuators import *
 from .sensors import *
 from .functional_modules import *
-from .dev_tools import *
